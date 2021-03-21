8f10bf4fca4e7a337a0e5b3727eff3337d8178ac
abedn2205 <abednego_ompusunggu@yahoo.com>
2021-03-21 23:46:36 +0700

    membuat form login dan getstarted

--- index.js
+++ index.js
@@ -7,5 +7,7 @@ import App from './src/App';
 import {name as appName} from './app.json';
 import Gaji from './src/pages/gaji';
 import Usaha from './src/pages/usaha';
+import Login from './src/pages/Login'
+import GetStarted from './src/pages/GetStarted'
 
-AppRegistry.registerComponent(appName, () => Usaha);
+AppRegistry.registerComponent(appName, () => GetStarted);
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,14 +1,50 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {ScrollView, StyleSheet, Text, View} from 'react-native';
+import {Gap, Input, Link, Button} from '../../component'
+import {colors} from '../../utility';
 
 const Login = () => {
   return (
-    <View>
-      <Text>Halaman Login</Text>
+    <View style={styles.pages}>
+      <Text style={styles.textInput}>Masuk dan mulai berkonsultasi</Text>
+      {/* <ScrollView showsVerticalScrollIndicator={false}> */}
+        <View style={styles.content}>
+          <Gap height={30}/>
+          <Input label="Email Address" />
+          <Gap height={20}/>
+          <Input label="Password"/>
+          <Gap height={10}/>
+          <Link title="Forget My Password"/>
+          <Gap height={20}/>
+          <Button title="Sign In"/>
+          <Gap height={30}/>
+          <Link title="Create New Account" align='center'/>
+        </View>
+      {/* </ScrollView> */}
     </View>
   );
 };
 
 export default Login;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  textInput: {
+    paddingLeft:38,
+    marginBottom:10,
+    marginTop:10,
+    width:320,
+    fontSize: 25,
+    fontWeight: 'bold',
+    textAlign:'left',        
+  },
+  content: {
+    padding: 40,
+    paddingTop: 0,
+  },
+  pages: {
+    backgroundColor: colors.white,
+    flex: 1,
+    justifyContent: 'center',
+    flexDirection: 'column',
+  },
+});
