a25d392972c3c36cdc81b1b09e0fb8cebe8cd5e0
Briva99 <bang.briva@gmail.com>
2021-03-21 14:29:48 +0700

    penambahan halaman form survey usaha

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
 import Gaji from './src/pages/gaji';
+import Usaha from './src/pages/usaha';
 
-AppRegistry.registerComponent(appName, () => Gaji);
+AppRegistry.registerComponent(appName, () => Usaha);
--- src/pages/usaha/index.js
+++ src/pages/usaha/index.js
@@ -1,19 +1,30 @@
 import React from 'react';
-import {ScrollView, StyleSheet, Text, View} from 'react-native';
-import {Gap, Header, Input} from '../../component';
+import {ScrollView, StyleSheet, View} from 'react-native';
+import {Button, Gap, Header, Input} from '../../component';
 import {colors} from '../../utility';
 
 const Usaha = ({navigation}) => {
   return (
     <View style={styles.pages}>
       <Header onPress={() => navigation.goBack()} title="Hasil Usaha" />
-      <ScrollView>
+      <ScrollView showsVerticalScrollIndicator={false}>
         <View style={styles.content}>
           <Gap height={20} />
           <Input label="Lokasi Usaha" />
           <Gap height={24} />
           <Input label="Omset" />
           <Gap height={24} />
+          <Input label="HPP" />
+          <Gap height={24} />
+          <Input label="Biaya Lain-lain" />
+          <Gap height={24} />
+          <Input label="GPS" />
+          <Gap height={24} />
+
+          <Button
+            title="Submit"
+            onPress={() => navigation.navigate('Report')}
+          />
         </View>
       </ScrollView>
     </View>
