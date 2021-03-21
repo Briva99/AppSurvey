34210114134ee06327b8640772f435b313c6a528
Briva99 <bang.briva@gmail.com>
2021-03-21 13:55:03 +0700

    menambah halamn list survey

---
+++ src/pages/ListSurvey/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const ListSurvey = () => {
+  return (
+    <View>
+      <Text>Halaman List Survey</Text>
+    </View>
+  );
+};
+
+export default ListSurvey;
+
+const styles = StyleSheet.create({});
