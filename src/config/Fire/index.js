a181654aed5e8acc20aac5a5d61f122bad19454e
Briva99 <bang.briva@gmail.com>
2021-03-28 20:47:56 +0700

    Add Configur Firebase

---
+++ src/config/Fire/index.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase';
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyDxZfRGTuvOoyXBYoLCQPGNf1qfg2losjg',
+  authDomain: 'appsurvey-1e861.firebaseapp.com',
+  databaseURL: 'https://appsurvey-1e861-default-rtdb.firebaseio.com',
+  projectId: 'appsurvey-1e861',
+  storageBucket: 'appsurvey-1e861.appspot.com',
+  messagingSenderId: '530427382112',
+  appId: '1:530427382112:web:ff267f70f9406e79847605',
+  measurementId: 'G-BG4ZPJ08V4',
+});
+
+const Fire = firebase;
+
+export default Fire;
